ea1c8e4da32aa8474e9dda7fe5f4656ee9240281
Инна <anni31717213@gmail.com>
2022-11-20 19:11:59 +0200

    залила готовий проект

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,49 @@
-export const App = () => {
+import { useState } from 'react';
+
+import Statistics from './Statistics/Statistics';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Section from './Section/Section';
+import Notification from './Notification/Notification';
+
+
+function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const updateState = (typeBtn) => {
+     return typeBtn === "good" ? setGood(good + 1) : typeBtn === "neutral" ? setNeutral(neutral + 1) : setBad(bad + 1);
+  }
+
+ const countTotalFeedback = () => {
+    return good + neutral + bad;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((good / countTotalFeedback()) * 100) || 0;
+  };
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={updateState}
+        />
+      </Section>
+      <Section title="Statistics">
+        {countTotalFeedback() ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
+          ></Statistics>
+        ) : (
+          <Notification message="No feedback given" />
+        )}
+      </Section>
+    </>
   );
-};
+}
+export default App;
---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+import css from './Section.module.css';
+
+const Section = ({ title, children }) => {
+  return (
+    <section className={css.section}>
+      <h2 className={css.title}>{title}</h2>
+      {children}
+    </section>
+  );
+};
+
+Section.defaultProps = {
+  title: "Section Title",
+};
+
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+export default Section;
+
